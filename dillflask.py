69e32e25921e9434975ba41178c9be6375ee91e4
Kevin Hu <hukevinxiaochen@gmail.com>
2016-04-08 20:20:26 -0400

    Create editor route.

--- dillflask.py
+++ dillflask.py
@@ -1,6 +1,12 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask import render_template
+
+@app.route("/editor")
+def editor():
+    return render_template("editor.html")
+
 @app.route("/")
 def hello():
     return "<h1 style='color:blue'>Hello World!</h1>"
