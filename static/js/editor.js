7372c238217eab47c2ae0295891b3d55a95d74a3
Kevin Hu <hukevinxiaochen@gmail.com>
2016-04-09 08:56:38 -0400

    Break down functions into basic jQuery event detectors.

--- static/js/editor.js
+++ static/js/editor.js
@@ -1,6 +1,11 @@
-var keysTyped = 0;
+// keyup event detector
 
 $( document ).on("keyup", "textarea[name='mdeditor']", function( event ) {
 	console.log(event.type);
 	console.log(event.keyCode);
+});
+
+// change event detector
+$( document ).on("change", "[name='mdeditor']", function (event) {
+	console.log(event.type);
 });
