f49a82936a5b7bf6ecebabcf4374e70db4ebf543
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-04-10 06:51:07 -0400

    Set up a sample React project.

---
+++ main.js
@@ -0,0 +1,29 @@
+//main.js
+require('es6-promise').polyfill();
+require('isomorphic-fetch');
+
+var React = require('react');
+var ReactDOM = require('react-dom');
+
+var Editor = React.createClass({
+    onEditorChg: function (event) {
+        console.log(event.type);
+        console.log(event.target.value);
+    },
+    render: function () {
+        return(
+            <div>
+            <form action="/login" method="POST">
+              <input type="text" name="officecode" />
+              <textarea name="mdinputarea" onChange={this.onEditorChg}></textarea>
+              <input type="submit" value="Submit" />
+            </form>
+            </div>
+        );
+    }
+});
+
+ReactDOM.render(
+    <Editor />,
+    document.getElementById('mdeditor')
+);
