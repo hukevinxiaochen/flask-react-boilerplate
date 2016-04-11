eb57206442e90edb7e04501d775dd3a099e5c6f2
hukevinxiaochen <hukevinxiaochen@gmail.com>
2016-04-11 19:54:08 -0400

    Reactify the Preview elements, and implemented a parent component called Interpreter that knows when the CHild Editor is changed.

--- static/main.js
+++ static/main.js
@@ -1,33 +1,64 @@
-//main.js
-// require('es6-promise').polyfill();
-// require('isomorphic-fetch');
+// main.js
+
+/*
+ * Front-end libraries we rely on.
+ * - React
+ * - markdown-it
+ *
+* */
 
 var React = require('react');
 var ReactDOM = require('react-dom');
+var md = require('markdown-it')();
 
-// Styles
-
+// React Components
 var editorStyle = {
     width: "100%"
 };
 
-// Components
 var Editor = React.createClass({
+    render: function () {
+        return(
+            <div className="one-half column">
+            <form action="/login" method="POST">
+            <textarea name="mdinputarea" onChange={this.props.chgHandler} style={editorStyle}></textarea>
+            <input type="submit" value="Submit" />
+            </form>
+            </div>
+        );
+    }
+});
+
+var Preview = React.createClass({
+    render: function () {
+        return(
+            <div className="one-half column">
+            {this.props.data}
+            </div>
+        );
+    }
+});
+
+var Interpreter = React.createClass({
+    getInitialState: function () {
+        return {data: []};
+    },
     onEditorChg: function (event) {
         console.log(event.type);
         console.log(event.target.value);
+        this.setState({data: [event.target.value]});
     },
     render: function () {
-        return(
-            <form action="/login" method="POST">
-            <textarea name="mdinputarea" onChange={this.onEditorChg} style={editorStyle}></textarea>
-            <input type="submit" value="Submit" />
-            </form>
+        return (
+            <div>
+            <Editor chgHandler={this.onEditorChg} />
+            <Preview data={this.state.data} />
+            </div>
         );
     }
 });
 
 ReactDOM.render(
-    <Editor />,
-    document.getElementById('mdeditor')
+    <Interpreter />,
+    document.getElementById('interpreter')
 );
