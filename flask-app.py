a3537036bdaf187865124db227c81d71aa609e82
Kevin Hu <hukevinxiaochen@gmail.com>
2016-05-20 12:56:07 -0400

    Reduce dill-flask.py to flask-app.py with just one route serving index.html.

--- flask-app.py
+++ flask-app.py
@@ -2,24 +2,16 @@ from flask import Flask
 from flask import render_template
 from flask import request
 
-app = Flask(__name__)
-
 # Debug mode. Be sure to comment out in production.
 # app.debug = True
 
-@app.route("/editor")
-def editor():
-    return render_template("editor.html")
-
-@app.route("/prev", methods = ['GET', 'POST'])
-def preview():
-    resp = "I'ma parse dis shit eventually" + request.data.decode()
-    return resp
+app = Flask(__name__)
 
 @app.route("/")
-def hello():
-    return "<h1 style='color:blue'>Hello World!</h1>"
+def index():
+    return render_template("index.html")
 
+# Logging Configuration
 if not app.debug:
     import logging
     from logging.handlers import RotatingFileHandler
