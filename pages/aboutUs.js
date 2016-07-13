37148197fe38b68a766099d615952281c807eb07
Kevin Hu <hukevinxiaochen@gmail.com>
2016-07-13 08:23:53 -0400

    Update aboutUs section with latest copy.

--- pages/aboutUs.js
+++ pages/aboutUs.js
@@ -4,24 +4,15 @@ var AboutUs = React.createClass({
     render: function () {
         return(
             <div className="content">
-                <img className="feature" src="../static/dancing.jpg" />
-                <p>Kevin and Erica first met during orientation week at Oberlin College. However, the exact timing of this momentous occasion is controversial. Erica remembers Kevin asking her to dance at the Orientation Swing Dance, but Kevin has no recollection of this event. He was first aware of Erica's existence while swimming at Chance Creek when he told her that she should not major in Biology. Fortunately, Erica did not take his advice, and never has since. Not a semester went by when they did not take at least one class together.</p>
-                <blockquote>
-                <p>Graduating</p>
-                </blockquote>
-                <blockquote>
-                <p>New York</p>
-                </blockquote>
-                <blockquote>
-                <p>Medical School</p>
-                </blockquote>
-                <ul>
-                <li>How we got engaged</li>
-                </ul>
-                <blockquote>
-                <p>After lengthy discussions about <em>their life philosophy</em>, Kevin and Erica schemed to bring both of their families together for Thanksgiving dinner in Lancaster where they announced their engagement.</p>
-                </blockquote>
-            </div>
+		    <h1>About Us</h1>
+		    <img className="feature" src="../static/dancing.jpg" />
+		    <h2>How we met</h2>
+		    <p>Kevin and Erica first met during orientation week at Oberlin College; however, the exact timing is controversial. Erica remembers Kevin asking her to dance at the orientation swing dance, but Kevin has no recollection of this. He remembers meeting Erica on a swimming trip at Chance Creek where he told her that she should not major in biology. Fortunately, Erica did not take his advice then, and never has since.
+		    </p>
+		    <h2>How we got engaged</h2>
+		    <p>After lengthy discussions about their life philosophy, Kevin and Erica schemed to bring both of their families together for Thanksgiving dinner in Lancaster where they announced their engagement.
+		    </p>
+	    </div>
         );
     }
 });
