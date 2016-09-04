24785e70dc61c5edc37bc6bc4653cd815804b4dc
Kevin Hu <hukevinxiaochen@gmail.com>
2016-09-03 22:45:59 -0400

    Sexify those form elements.

--- pages/updates.js
+++ pages/updates.js
@@ -1,38 +1,44 @@
 import React from 'react';
-
-// TODO: Style it up!
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
 
 var Updates = React.createClass({
     render: function () {
 	    return (
 		    <div className="content">
 			    <div id="mc_embed_signup">
-				    <form action="//ericakevin.us13.list-manage.com/subscribe/post?u=5557db02a80ebaa4bdb958f2f&amp;id=54edacb37c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" novalidate>
+				    <form action="//ericakevin.us13.list-manage.com/subscribe/post?u=5557db02a80ebaa4bdb958f2f&amp;id=54edacb37c" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" className="validate" target="_blank" noValidate>
 					    <div id="mc_embed_signup_scroll">
-						    <p>Share your email with us if you would like to receive notice of updates.</p>
-						    <div className="indicates-required"><span className="asterisk">*</span> indicates required</div>
-						    <div className="mc-field-group">
-							    <label for="mce-EMAIL">Email Address  <span className="asterisk">*</span></label>
+						    <p>Share your email with us if you would like to receive notice of major updates!</p>
+						    {/*<div className="indicates-required"><span className="asterisk">*</span> indicates required</div>*/}
+						    {/*<div className="mc-field-group">
+							    <label htmlFor="mce-EMAIL">Email Address  <span className="asterisk">*</span></label>
 							    <input type="email" name="EMAIL" className="required email" id="mce-EMAIL" />
-						    </div>
-						    <div className="mc-field-group">
-							    <label for="mce-FNAME">Given Name </label>
+							    </div>*/}
+							    <TextField hintText="Email" type="email" name="EMAIL" id="mce-EMAIL" />
+							    <br />
+						    {/*<div className="mc-field-group">
+							    <label htmlFor="mce-FNAME">Given Name </label>
 							    <input type="text" name="FNAME" className="" id="mce-FNAME" />
-						    </div>
-						    <div className="mc-field-group">
-							    <label for="mce-LNAME">Family Name </label>
+							    </div>*/}
+							    <TextField hintText="Given Name" type="text" name="FNAME" id="mce-FNAME" />
+							    <br />
+						    {/*<div className="mc-field-group">
+							    <label htmlFor="mce-LNAME">Family Name </label>
 							    <input type="text" name="LNAME" className="" id="mce-LNAME" />
-						    </div>
+							    </div>*/}
+						    <TextField hintText="Last Name" type="text" name="LNAME" id="mce-LNAME" />
 						    <div id="mce-responses" className="clear">
 							    <div className="response" id="mce-error-response" style={{display: "none"}}></div>
 							    <div className="response" id="mce-success-response" style={{display: "none"}}></div>
 						    </div>
 						    <div style={{position: "absolute", left: -5000}} aria-hidden="true">
-							    <input type="text" name="b_5557db02a80ebaa4bdb958f2f_54edacb37c" tabindex="-1" value="" />
+							    <input type="text" name="b_5557db02a80ebaa4bdb958f2f_54edacb37c" tabIndex="-1" value="" />
 						    </div>
-						    <div className="clear">
+						    {/*<div className="clear">
 							    <input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" className="button" />
-						    </div>
+							    </div>*/}
+							    <RaisedButton label="Subscribe" primary={true} type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" />
 					    </div>
 				    </form>
 			    </div>
