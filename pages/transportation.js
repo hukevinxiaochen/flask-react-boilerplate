105c1f0087ddc23613ff2222291dd05829572560
Kevin Hu <hukevinxiaochen@gmail.com>
2016-09-04 07:34:15 -0400

    Put in links for transportation resources.

--- pages/transportation.js
+++ pages/transportation.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { List, ListItem } from 'material-ui/List'; 
 import { Link } from 'react-router';
-// TODO: Link it up!
+
 var Transportation = React.createClass({
     render: function () {
         return (
@@ -9,10 +9,18 @@ var Transportation = React.createClass({
 		    <h1>Transportation</h1>
 		    <h2>To and From Lancaster</h2>
 		    <List style={{marginRight: "auto", marginLeft: "auto", maxWidth: 600}}>
-			    <ListItem primaryText="Lancaster Amtrak Station (LNC) (service to/from Philadelphia and New York City on the Keystone)" />
-			    <ListItem primaryText="Harrisburg International Airport (MDT) (½ hour drive)" />
-			    <ListItem primaryText="Baltimore Washington International (BWI) (1 ½ hour drive)" />
-			    <ListItem primaryText="Philadelphia International Airport (PHL) (1 ½ hour drive)" />
+			    <a href="https://www.amtrak.com/servlet/ContentServer?pagename=am/am2Station/Station_Page&code=LNC">
+				    <ListItem primaryText="Lancaster Amtrak Station (LNC) (service to/from Philadelphia and New York City on the Keystone)" />
+			    </a>
+			    <a href="http://www.flyhia.com/">
+				    <ListItem primaryText="Harrisburg International Airport (MDT) (½ hour drive)" />
+			    </a>
+			    <a href="http://www.bwiairport.com/en">
+				    <ListItem primaryText="Baltimore Washington International Airport (BWI) (1 ½ hour drive)" />
+			    </a>
+			    <a href="http://www.phl.org/Pages/HomePage.aspx">
+				    <ListItem primaryText="Philadelphia International Airport (PHL) (1 ½ hour drive)" />
+			    </a>
 		    </List>
 
 		    <h2>Around Lancaster</h2>
@@ -20,8 +28,7 @@ var Transportation = React.createClass({
 		{/*<h2>To and From Lancaster</h2>
 		   <h3>Uber is in Lancaster</h3>
 		*/}
-		{/*<h2>To and From Ceremony</h2>*/}
-		{/* TODO: Link to sign up for updates */}
+		{/*<h2>To and From Ceremony</h2>*/}	
             </div>
         );
     }
