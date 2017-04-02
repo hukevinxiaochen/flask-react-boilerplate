b64a24005bfcd8f7313766111255ce62dd1bdc9d
Kevin Hu <hukevinxiaochen@gmail.com>
2017-04-02 10:37:39 -0400

    Must revalidate.

--- flaskapp.py
+++ flaskapp.py
@@ -1,16 +1,26 @@
 from flask import Flask
 from flask import render_template
 from flask import request
-
-# Debug mode. Be sure to comment out in production.
-# app.debug = True
+from flask import make_response
 
 app = Flask(__name__)
 
+# Debug mode. Be sure to comment out in production.
+# app.debug = True
 @app.route("/")
 @app.route("/<page_name>")
 def index(page_name=None):
-    return render_template("index.html")
+    resp = make_response(render_template("index.html"))
+    resp.headers["Cache-Control"] = "must-revalidate"
+    return resp
+
+
+# Stack Overflow
+# http://stackoverflow.com/questions/23112316/using-flask-how-do-i-modify-the-cache-control-header-for-all-output#23115561
+@app.after_request
+def apply_caching(resp):
+    resp.headers["Cache-Control"] = "max-age=0, must-revalidate"
+    return resp
 
 # Logging Configuration
 if not app.debug:
